d445f71b05e6210dfaa6ef1fa42492d3ac3d6ba9
Armin Sabet <armin.sabet@fortescue.com>
2024-10-31 12:13:20 +0800

    First promising output

--- blendmaster-backend/routes/optimization_engine.py
+++ blendmaster-backend/routes/optimization_engine.py
@@ -1,14 +1,16 @@
 from routes.optimization_logic import run_blending_optimization
 from time_handler import depletion_time_tracker
 
-def run_with_dynamic_steady_state(event_pool, period_crusher_target, period, periods):
+def run_with_dynamic_steady_state(event_pool, period_crusher_target, periods, current_time):
     # Step 1: Set the initial steady state duration
-    if period == "preplan":
-        steady_state_duration = (periods["preplan_end"] - periods["preplan_start"]).total_seconds() / 3600
-    elif period == "period_1":
-        steady_state_duration = 12
-    elif period == "period_2":
-        steady_state_duration = 12
+    
+    if current_time >= periods["preplan_start"] and current_time < periods["preplan_end"]: 
+        steady_state_duration = (periods["preplan_end"] - current_time).total_seconds() / 3600
+    elif current_time >= periods["period_1_start"] and current_time < periods["period_1_end"]:
+        steady_state_duration = (periods["period_1_end"] - current_time).total_seconds() / 3600 
+    elif current_time >= periods["period_2_start"] and current_time < periods["period_2_end"]:
+        steady_state_duration = (periods["period_2_end"] - current_time).total_seconds() / 3600
+    else: steady_state_duration = 0
     
     # Step 2: Run the initial optimization
     result = run_blending_optimization(event_pool, period_crusher_target, steady_state_duration)
